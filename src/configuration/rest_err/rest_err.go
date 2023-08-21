caf26738a72413fa6ab5371696783f90e87d40a7
neto <neto@F680V9.0>
2023-08-20 21:36:29 -0300

    feat:  struct RestErr

--- src/configuration/rest_err/rest_err.go
+++ src/configuration/rest_err/rest_err.go
@@ -1,5 +1,7 @@
 package rest_test
 
+import "net/http"
+
 type RestErr struct {
 	Message string   `json:"message"`
 	Err     string   `json:"error"`
@@ -18,5 +20,58 @@ func NewRestErr(message, err string, code int, causes []Causes) *RestErr {
 		Err:     err,
 		Code:    code,
 		Causes:  causes,
+	}
+}
+
+func (r *RestErr) Error() string {
+	return r.Message
+}
+
+func NewBadRequest(message, err string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_resquest",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+func NewUnauthorizedRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "unauthorized",
+		Code:    http.StatusUnauthorized,
+	}
+}
+
+func NewBadRequestValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "not_found",
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewForbiddenError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "forbidden",
+		Code:    http.StatusForbidden,
 	}
 }
